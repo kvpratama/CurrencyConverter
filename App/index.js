0f6bdbfc0556be2dda39f36fbc556f50c0d1f097
Kevin Pratama <kvpratama@gmail.com>
2022-01-01 15:15:49 +0900

    finish module 5

--- App/index.js
+++ App/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Options from "./screens/Options";
-import Home from "./screens/Home";
+import Navigation from "./config/Navigation";
 
-export default () => <Home />;
+export default () => <Navigation />;
